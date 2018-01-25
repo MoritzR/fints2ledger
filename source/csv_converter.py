dee08826d89c1449b240349cd3bc08f71ff6dbae
Moritz Rumpf <moritz.rumpf@leanovate.de>
2018-01-25 12:43:16 +0100

    Add converter to csv
    Add converter to csv. Can be used in conjunction with tools like reckon or csv2ledger convert to ledger transactions.

---
+++ source/csv_converter.py
@@ -0,0 +1,12 @@
+class CsvConverter:
+    def convert(self, transaction):
+        hbciData = transaction.data
+
+        date = hbciData["date"].strftime("%Y/%m/%d")
+        amount = str(hbciData["amount"].amount)
+        currency = "€" if hbciData["amount"].currency == "EUR" else hbciData["amount"].currency
+        posting_text = hbciData["posting_text"]
+        applicant_name = hbciData["applicant_name"]
+        purpose = hbciData["purpose"]
+        print([date, amount, currency, applicant_name, posting_text, purpose])
+        return ",".join([date, amount, currency, applicant_name, posting_text, purpose])
---
+++ source/csv_converter_test.py
@@ -0,0 +1,33 @@
+import unittest
+from csv_converter import CsvConverter
+from mt940.models import Amount, Date
+
+class ConverterTest(unittest.TestCase):
+    def setUp(self):
+        self.csvConverter = CsvConverter()
+
+    def test_convert_to_csv(self):
+        expected_purpose = "purchased book"
+        expected_applicant_name="book store"
+        expected_posting_text="posting text"
+        expected_date=Date(2017, 11, 1)
+        expected_amount=Amount('44', 'D', 'EUR')
+        fintsTransaction = {
+            "date": expected_date,
+            "amount": expected_amount,
+            "applicant_name": expected_applicant_name,
+            "posting_text": expected_posting_text,
+            "purpose": expected_purpose
+        }
+
+        csv_result = self.csvConverter.convert(fintsTransaction)
+        entries=csv_result.split(",")
+
+        self.assertIn(expected_purpose, entries)
+        self.assertIn(expected_applicant_name, entries)
+        self.assertIn(expected_posting_text, entries)
+        self.assertIn("2017/11/01", entries)
+        self.assertIn("-44", entries)
+
+if __name__ == '__main__':
+    unittest.main()
