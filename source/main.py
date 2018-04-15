8eab2affa3f03d8bec4a7ba80804480160ef10ec
Moritz Rumpf <moritz.rumpf@gmail.com>
2018-04-15 13:07:18 +0200

    Add example usage of LedgerWriter

--- source/main.py
+++ source/main.py
@@ -3,6 +3,8 @@ from mt940.models import Date
 from fints.client import FinTS3PinTanClient
 from csv_converter import CsvConverter
 import configparser
+from ledger_writer import LedgerWriter
+import csv
 
 '''
 This requires a "application.config" file in the same folder, according to the following format:
@@ -27,11 +29,27 @@ def retrieveAndSave():
 
     retriever = TRetriever(client, fintsConfig["account"])
     converter = CsvConverter(";")
-    today = Date.today()
-    csv = "\n".join(map(lambda transaction: converter.convert(
-        transaction), retriever.get_hbci_transactions(Date(2017, 12, 18), Date.today())))
+    csv_output = "\n".join(map(lambda transaction: converter.convert(
+        transaction), retriever.get_hbci_transactions(Date(2018, 3, 25), Date.today())))
     with open('transaction.csv', 'w') as f:
-        f.write(csv)
+        f.write(converter.get_headline())
+        f.write("\n")
+        f.write(csv_output)
+
+
+def convertToLedger():
+    writer = LedgerWriter()
+    with open('transaction.csv') as csvfile, open('transactions.ledger', 'a') as ledger_journal:
+        reader = csv.DictReader(csvfile, delimiter=";")
+        for row in reader:
+            row.update({
+                "debit_account": "test:debit",
+                "credit_account": "test:credit"
+            })
+            entry = writer.journal_entry(row)
+            ledger_journal.write(entry)
+            ledger_journal.write("\n")
 
 
 retrieveAndSave()
+convertToLedger()
