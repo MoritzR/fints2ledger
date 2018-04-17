44ab8e3099057bf7a701774cadec475d73b493de
Moritz Rumpf <moritz.rumpf@gmail.com>
2018-04-17 20:38:48 +0200

    Move setup.py out of source code folder

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='fints2ledger',
       version='0.1',
-      description='Pulling fromt fints apis and converting to ledger',
+      description='A tool for downloading transactions from FinTS banking APIs and sorting them into a ledger journal.',
       url='https://github.com/MoritzR/fints2ledger',
       author='Moritz Rumpf',
       license='MIT',
@@ -10,5 +10,6 @@ setup(name='fints2ledger',
           'mt-940',
           'fints'
       ],
+      packages=['fints2ledger'],
       dependency_links=['https://github.com/raphaelm/python-fints/tarball/master/#egg=fints-0.2.1'],
       zip_safe=False)
