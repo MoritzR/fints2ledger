33c52d7af573b5cdb047c6885005002ee51903de
Moritz Rumpf <moritz.rumpf@gmail.com>
2018-04-22 17:46:46 +0200

    Add long description from readme to package description

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='fints2ledger',
       version='0.1',
       description='A tool for downloading transactions from FinTS banking APIs and sorting them into a ledger journal.',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
       url='https://github.com/MoritzR/fints2ledger',
       author='Moritz Rumpf',
       license='MIT',
