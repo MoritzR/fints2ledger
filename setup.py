f89a75ba4f17b3610ec7726d190bcf2eb82ebb1d
Moritz Rumpf <moritz.rumpf@gmail.com>
2019-08-21 20:31:04 +0200

    Add python 3.7 as supported version label
    travis tests run successfully for 3.7

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(name='fints2ledger',
           "Development Status :: 3 - Alpha",
           "Programming Language :: Python :: 3.4",
           "Programming Language :: Python :: 3.5",
-          "Programming Language :: Python :: 3.6"
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7"
       ]
       )
