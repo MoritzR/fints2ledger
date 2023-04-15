a4da108219c20de20eda895ec7b32ebc12458f6a
Moritz Rumpf <moritz.rumpf@gmail.com>
2023-04-15 11:19:23 +0200

    Test decimal parser
    Resolves two TODOs

--- src/Matching/Parser.hs
+++ src/Matching/Parser.hs
@@ -1,11 +1,9 @@
-module Matching.Parser (amountParser, runParser) where
+module Matching.Parser (amountParser, runParser, endOnDecimal) where
 
 import Control.Applicative (optional)
-import Data.Char (isDigit)
 import Data.Maybe (listToMaybe)
 import Text.ParserCombinators.ReadP (ReadP, char, choice, munch1, pfail, readP_to_S, string)
 import Text.Read (readMaybe)
-import Utils ((??))
 
 runParser :: ReadP a -> String -> Maybe a
 runParser parser = listToMaybe . map fst . readP_to_S parser
@@ -24,18 +22,13 @@ amountParser :: ReadP (Double -> Bool)
 amountParser = do
   comparison <- choice $ map (\(s, f) -> f <$ string s) comparisons
   optional space
-  number <- decimal
+  number <- endOnDecimal
   return (`comparison` number)
 
 space :: ReadP Char
 space = char ' '
 
--- TODO test
-decimal :: ReadP Double
-decimal = do
-  beforeDot <- munch1 isDigit
-  afterDot <- optional do
-    char '.'
-    munch1 isDigit
-  let parsedNumber = readMaybe $ beforeDot ++ "." ++ (afterDot ?? "")
-  maybe pfail return parsedNumber
+endOnDecimal :: ReadP Double
+endOnDecimal = do
+  allRemainingCharacters <- munch1 $ const True
+  maybe pfail return (readMaybe allRemainingCharacters)
--- test/Matching/ParserSpec.hs
+++ test/Matching/ParserSpec.hs
@@ -2,20 +2,26 @@ module Matching.ParserSpec (spec) where
 
 import Data.Foldable (forM_)
 import Matching.Parser qualified as P
+import Test.QuickCheck (property)
 import Test.Syd (Spec, describe, it, shouldBe)
 
 spec :: Spec
 spec = do
+  describe "endOnDecimal" do
+    let runParswerWith = P.runParser P.endOnDecimal
+    it "parses all doubles that end a string" do
+      property \(number :: Double) ->
+        runParswerWith (show number) `shouldBe` Just number
+
   describe "Parsing amount matches" do
     let runParserWith number = P.runParser (fmap ($ number) P.amountParser)
     let tests =
-          -- TODO test negative values
-          [ ("<= 99.2", [80, 99.2], [99.3, 100])
-          , ("< 99.2", [80], [99.2, 99.3, 100])
+          [ ("<= 99.2", [-100, 80, 99.2], [99.3, 100])
+          , ("< 99.2", [-100, 80], [99.2, 99.3, 100])
           , ("= 99.2", [99.2], [-100, 80, 99.3, 100])
           , ("99.2", [99.2], [-100, 80, 99.3, 100])
-          , (">= 99.2", [99.2, 99.3, 100], [80])
-          , ("> 99.2", [99.3, 100], [80, 99.2])
+          , (">= 99.2", [99.2, 99.3, 100], [-100, 80])
+          , ("> 99.2", [99.3, 100], [-100, 80, 99.2])
           ]
     forM_ tests \(toParse, examples, counterExamples) -> do
       describe ("parsing '" ++ toParse ++ "'") do
