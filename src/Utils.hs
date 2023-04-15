d28ba88ef60df0dbe21eaa5d6142101e44cff3f7
Moritz Rumpf <moritz.rumpf@gmail.com>
2023-04-15 22:23:35 +0200

    Updated workflow for stack
    Move md5 generation to utils
    
    Fix md5 being calulated incorrectly
    
    because number where formatted differently than in the python version
    
    Add TODO

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,13 +1,17 @@
-module Utils (encodeAsString, byteStringToString, printEmptyLine, (??), orElseThrow, createFile) where
+module Utils (encodeAsString, byteStringToString, printEmptyLine, (??), orElseThrow, createFile, calculateMd5Value, formatDouble) where
 
 import Control.Exception (Exception, throwIO)
+import Crypto.Hash.MD5 qualified as MD5
 import Data.Aeson (ToJSON, encode)
+import Data.ByteString.Base16 qualified as Base16
 import Data.ByteString.Lazy (ByteString)
-import Data.Text.Lazy (unpack)
-import Data.Text.Lazy.Encoding (decodeUtf8)
+import Data.Text.Encoding qualified as T
+import Data.Text.Lazy (Text)
+import Data.Text.Lazy qualified as TL
+import Data.Text.Lazy.Encoding qualified as TL
 
 byteStringToString :: ByteString -> String
-byteStringToString = unpack . decodeUtf8
+byteStringToString = TL.unpack . TL.decodeUtf8
 
 encodeAsString :: ToJSON a => a -> String
 encodeAsString = byteStringToString . encode
@@ -26,3 +30,13 @@ orElseThrow eitherAB toException = case eitherAB of
 
 createFile :: FilePath -> IO ()
 createFile path = writeFile path ""
+
+calculateMd5Value :: [Text] -> Text
+calculateMd5Value md5Values = TL.fromStrict $ T.decodeUtf8 $ Base16.encode $ MD5.finalize $ foldl MD5.update MD5.init (T.encodeUtf8 . TL.toStrict <$> md5Values)
+
+formatDouble :: Double -> String
+formatDouble double
+  | fromInteger floored == double = show floored
+  | otherwise = show double
+ where
+  floored = floor double :: Integer
