05fa48a129ae81dd6516dc6fecb32c4a6ddeb684
Moritz Rumpf <moritz.rumpf@gmail.com>
2023-04-22 21:23:01 +0200

    Remove redundant imports

--- test/UtilsSpec.hs
+++ test/UtilsSpec.hs
@@ -1,11 +1,6 @@
 module UtilsSpec (spec) where
 
-import Data.Aeson qualified as Aeson
-import Data.Text.Lazy.Encoding (encodeUtf8)
-import Data.Text.Lazy.IO as TLIO
-import Paths_hsfints2ledger (getDataFileName)
 import Test.Syd (Spec, describe, it, shouldBe)
-import Transactions (Amount (Amount), Transaction (..))
 import Utils (formatDouble)
 
 spec :: Spec
