7d53f3cee1341916411eb03140415bd019fa7933
Moritz Rumpf <moritz.rumpf@gmail.com>
2023-04-26 20:45:07 +0200

    Make completions not show prefix
    This makes is so that completing "expenses:monthly"
    
    prints
    "rent"
    
    instead of
    "expenses:monthly:rent"
    
    This makes it easier to see what will be completed, especially when there are lots of completions

--- src/Completion.hs
+++ src/Completion.hs
@@ -5,11 +5,13 @@ where
 
 import Config.AppConfig (AppConfig (..))
 import Data.Function ((&))
+import Data.List (stripPrefix)
 import Data.Text (isPrefixOf, pack, unpack)
 import Data.Text.Lazy (Text)
 import Hledger (Journal)
 import Ledger (getAccounts, getJournal)
-import System.Console.Haskeline (Completion, CompletionFunc, InputT, Settings (Settings), completeWord, noCompletion, runInputT, simpleCompletion)
+import System.Console.Haskeline (Completion (Completion), CompletionFunc, InputT, Settings (Settings), completeWord, noCompletion, runInputT)
+import Utils ((??))
 
 runCompletion :: AppConfig -> Text -> (forall a. InputT IO a -> IO a)
 runCompletion config key
@@ -21,7 +23,9 @@ getCompletions :: Journal -> String -> [Completion]
 getCompletions journal input =
   getAccounts journal
     & filter (isPrefixOf $ pack input)
-    & map (simpleCompletion . unpack)
+    & map (completion . unpack)
+ where
+  completion string = Completion string (stripPrefix input string ?? string) False
 
 accountCompletion :: Journal -> CompletionFunc IO
 accountCompletion journal = completeWord Nothing [] (return . getCompletions journal)
