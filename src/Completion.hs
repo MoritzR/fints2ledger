7f1329cd4c883b2b2dd283bcd897b8092ec5f19b
Moritz Rumpf <moritz.rumpf@gmail.com>
2023-06-07 11:18:04 +0200

    Refactor more Text usage
    as Text is the dominate string type used here

--- src/Completion.hs
+++ src/Completion.hs
@@ -5,8 +5,7 @@ where
 
 import Config.AppConfig (AppConfig (..))
 import Data.Function ((&))
-import Data.List (stripPrefix)
-import Data.Text (Text, isPrefixOf, pack, unpack)
+import Data.Text (Text, isPrefixOf, pack, stripPrefix, unpack)
 import Hledger (Journal)
 import Ledger (getAccounts, getJournal)
 import System.Console.Haskeline (Completion (Completion), CompletionFunc, InputT, Settings (Settings), completeWord, noCompletion, runInputT)
@@ -24,18 +23,19 @@ runWithAccountCompletion config input = do
   journal <- getJournal config.journalFile
   runInputT (makeSettings $ accountCompletion journal) input
  where
-  accountCompletion journal = completeWord Nothing [] (return . getCompletions journal)
+  accountCompletion journal = completeWord Nothing [] (return . getCompletions journal . pack)
 
 runWithNoCompletion :: InputT IO a -> IO a
 runWithNoCompletion = runInputT $ makeSettings noCompletion
 
-getCompletions :: Journal -> String -> [Completion]
+getCompletions :: Journal -> Text -> [Completion]
 getCompletions journal input =
   getAccounts journal
-    & filter (isPrefixOf $ pack input)
-    & map (completion . unpack)
+    & filter isPrefixOfInput
+    & map completion
  where
-  completion string = Completion string (stripPrefix input string ?? string) False
+  isPrefixOfInput = isPrefixOf input
+  completion account = Completion (unpack account) (unpack $ stripPrefix input account ?? account) False
 
 makeSettings :: CompletionFunc m -> Settings m
 makeSettings completion = Settings completion Nothing False
