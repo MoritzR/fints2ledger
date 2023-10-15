99670f7d5a3bd5dcae1427e5abcd1990b27cb330
Moritz Rumpf <moritz.rumpf@gmail.com>
2023-10-15 23:02:03 +0200

    Read template file from config directory
    for easier customization.
    If not present the default template will be used.

--- src/Config/Files.hs
+++ src/Config/Files.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-module Config.Files (getDefaultConfigDirectory, getConfigFilePath, templateFile, exampleFile, pyfintsFile, ConfigDirectory (..)) where
+module Config.Files (getDefaultConfigDirectory, getConfigFilePath, getTemplateFile, exampleFile, pyfintsFile, ConfigDirectory (..)) where
 
 import Data.ByteString (ByteString)
 import Data.FileEmbed (embedDir)
@@ -9,7 +9,7 @@ import Data.Map (Map, fromList, (!))
 import Data.String (IsString)
 import Data.Text (Text)
 import Data.Text.Encoding qualified as T
-import System.Directory (getHomeDirectory)
+import System.Directory (doesFileExist, getHomeDirectory)
 import System.FilePath ((</>))
 
 getDefaultConfigDirectory :: IO ConfigDirectory
@@ -20,8 +20,13 @@ getDefaultConfigDirectory = do
 getConfigFilePath :: ConfigDirectory -> FilePath
 getConfigFilePath configDirectory = configDirectory.get </> "config.yml"
 
-templateFile :: Text
-templateFile = T.decodeUtf8 $ dataFiles ! "template.txt"
+getTemplateFile :: ConfigDirectory -> (FilePath -> IO Text) -> IO Text
+getTemplateFile configDirectory readTextFile = do
+  let templateFilePath = configDirectory.get </> "template.txt"
+  fileExists <- doesFileExist templateFilePath
+  if fileExists
+    then readTextFile templateFilePath
+    else return $ T.decodeUtf8 $ dataFiles ! "template.txt"
 
 exampleFile :: Text
 exampleFile = T.decodeUtf8 $ dataFiles ! "example.json"
