4745f547af66dad380622950e0038586b05e3d2d
Moritz Rumpf <moritz.rumpf@gmail.com>
2025-03-13 14:44:56 +0100

    Remove sydtest-discover
    For some reason that was not working with my cabal (ghc: could not
    execute sydtest-discover) which no further useful output.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,1 +1,20 @@
-{-# OPTIONS_GHC -F -pgmF sydtest-discover #-}
+module Main (main) where
+
+import Test.Syd
+import qualified ConfigSpec
+import qualified Dates.ParserSpec
+import qualified Matching.MatchingSpec
+import qualified Matching.ParserSpec
+import qualified PromptSpec
+import qualified TransactionSpec
+import qualified UtilsSpec
+
+main :: IO ()
+main = sydTest $ describe "fints2ledger" do
+  ConfigSpec.spec
+  Dates.ParserSpec.spec
+  Matching.MatchingSpec.spec
+  Matching.ParserSpec.spec
+  PromptSpec.spec
+  TransactionSpec.spec
+  UtilsSpec.spec
