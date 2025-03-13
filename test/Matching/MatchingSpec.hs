e8836acd2da2f3df3b009b156403effd9786186c
Moritz Rumpf <moritz.rumpf@gmail.com>
2025-03-13 16:36:03 +0100

    Make app compile on ghc 9.8

--- test/Matching/MatchingSpec.hs
+++ test/Matching/MatchingSpec.hs
@@ -1,6 +1,6 @@
 module Matching.MatchingSpec (spec) where
 
-import Config.YamlConfig (Filling (Filling, match), fill)
+import Config.YamlConfig (Filling (Filling, match, fill))
 import Data.Map (empty, fromList)
 import Matching.Matching (findMatch)
 import Test.Syd (Spec, describe, it, shouldBe)
