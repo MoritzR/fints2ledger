75bbbd6fd0c232becbb816c4101dc2c10bd1d712
Moritz Rumpf <moritz.rumpf@gmail.com>
2025-03-24 16:43:52 +0100

    Add nix build setup

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs = inputs@{ nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux" ];
+      imports = [ ];
+
+      perSystem = { pkgs, ... }: {
+        packages.default = pkgs.haskellPackages.developPackage {
+        # packages.default = pkgs.haskell.packages.ghc910.developPackage {
+          root = ./.;
+          source-overrides = {
+            text-format-heavy = pkgs.fetchFromGitHub {
+              owner = "MoritzR";
+              repo = "text-format-heavy";
+              rev = "d7de69f3e2e931ed56a67bbbd2608f778c714302";
+              hash = "sha256-OTty1uHIakaKHGfphNBiSzmIn/XDcllDzeaznuKqd5M=";
+            };
+          };
+        };
+      };
+    };
+}
