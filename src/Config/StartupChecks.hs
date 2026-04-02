4ad62593dc01cbd2fedde77f8fcad03a841428b4
Moritz Andrich <moritz.rumpf@gmail.com>
2026-04-02 20:35:22 +0200

    Add inital UI for selecting banking endpoint

--- src/Config/StartupChecks.hs
+++ src/Config/StartupChecks.hs
@@ -1,11 +1,19 @@
+{-# LANGUAGE OverloadedLabels #-}
+
 module Config.StartupChecks (runStartupChecks) where
 
+import Config.Banks (fintsEndpoint)
 import Config.CliConfig (CliConfig (..))
 import Config.Files (ConfigDirectory (..), getConfigFilePath)
 import Config.YamlConfig (defaultYamlConfig, writeYamlConfig)
 import Control.Exception (Exception, throwIO)
+import Control.Lens ((.~))
 import Control.Monad (unless)
+import Data.Function ((&))
+import Data.Generics.Labels ()
+import Data.Text (pack)
 import System.Directory (createDirectoryIfMissing, doesFileExist)
+import UI.BankSelectionUI (runBankSelectionUI)
 import UI.ConfigUI (runConfigUI)
 
 newtype ConfigSetupAborted = ConfigSetupAborted String deriving (Show)
@@ -19,7 +27,12 @@ runStartupChecks cliConfig = do
   let configFilePath = getConfigFilePath cliConfig.configDirectory
   configFileExists <- doesFileExist configFilePath
   unless configFileExists do
-    maybeConfig <- runConfigUI defaultYamlConfig cliConfig.configDirectory
-    case maybeConfig of
-      Just config -> writeYamlConfig configFilePath config
-      Nothing -> throwIO $ ConfigSetupAborted "Please fill out and save the config form before continuing."
+    maybeBank <- runBankSelectionUI
+    case maybeBank of
+      Nothing -> throwIO $ ConfigSetupAborted "Please select a bank before continuing."
+      Just bank -> do
+        let initialConfig = defaultYamlConfig & #fints . #endpoint .~ pack (fintsEndpoint bank)
+        maybeConfig <- runConfigUI initialConfig cliConfig.configDirectory
+        case maybeConfig of
+          Just config -> writeYamlConfig configFilePath config
+          Nothing -> throwIO $ ConfigSetupAborted "Please fill out and save the config form before continuing."
---
+++ src/UI/BankSelectionUI.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE OverloadedLabels #-}
+
+module UI.BankSelectionUI (runBankSelectionUI) where
+
+import Brick (AttrMap, BrickEvent (VtyEvent), Padding (Pad), Widget, attrMap, customMain, halt, on, padAll, padTop, str, zoom, (<=>))
+import Brick.Main qualified as Brick (App (..))
+import Brick.Widgets.List (List, handleListEvent, list, listSelectedElement, listSelectedFocusedAttr, renderList)
+import Config.Banks (Bank (..))
+import Control.Lens ((.=))
+import Data.Generics.Labels ()
+import Data.Vector qualified as Vec
+import GHC.Generics (Generic)
+import Graphics.Vty qualified as V
+import Graphics.Vty.Config qualified
+import Graphics.Vty.CrossPlatform qualified
+
+data BankSelectionState = BankSelectionState
+  { bankList :: List () Bank
+  , aborted :: Bool
+  }
+  deriving (Generic)
+
+allBanks :: [Bank]
+allBanks = [ING, GLS, OTHER]
+
+displayName :: Bank -> String
+displayName ING = "ING"
+displayName GLS = "GLS"
+displayName OTHER = "Other (enter endpoint manually)"
+
+runBankSelectionUI :: IO (Maybe Bank)
+runBankSelectionUI = do
+  let initialState =
+        BankSelectionState
+          { bankList = list () (Vec.fromList allBanks) 1
+          , aborted = False
+          }
+  initialVty <- buildVty
+  finalState <-
+    customMain
+      initialVty
+      buildVty
+      Nothing
+      bankSelectionApp
+      initialState
+  if finalState.aborted
+    then return Nothing
+    else return $ snd <$> listSelectedElement finalState.bankList
+
+draw :: BankSelectionState -> [Widget ()]
+draw state =
+  [ padAll 1 $
+      str "Select your bank:"
+        <=> renderList renderItem True state.bankList
+        <=> padTop (Pad 1) (str "[↑↓] Navigate  [Enter] Select  [Ctrl+C] Quit")
+  ]
+ where
+  renderItem _isSelected bank = str (displayName bank)
+
+attributeMap :: AttrMap
+attributeMap =
+  attrMap
+    V.defAttr
+    [(listSelectedFocusedAttr, V.black `on` V.yellow)]
+
+bankSelectionApp :: Brick.App BankSelectionState e ()
+bankSelectionApp =
+  Brick.App
+    { appDraw = draw
+    , appHandleEvent = \case
+          VtyEvent (V.EvKey (V.KChar 'c') [V.MCtrl]) -> do
+            #aborted .= True
+            halt
+          VtyEvent (V.EvKey V.KEnter []) -> halt
+          VtyEvent vtyEvent -> zoom #bankList $ handleListEvent vtyEvent
+          _otherwise -> return ()
+    , appChooseCursor = \_ _ -> Nothing
+    , appStartEvent = return ()
+    , appAttrMap = const attributeMap
+    }
+
+buildVty :: IO V.Vty
+buildVty = do
+  v <- Graphics.Vty.CrossPlatform.mkVty Graphics.Vty.Config.defaultConfig
+  V.setMode (V.outputIface v) V.Mouse True
+  return v
