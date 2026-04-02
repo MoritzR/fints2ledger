f5872c3ee3ff194b592c66256f9b124fcd77b066
Moritz Andrich <moritz.rumpf@gmail.com>
2026-04-02 20:00:04 +0200

    Add file for banks that we know the endpoint of

---
+++ src/Config/Banks.hs
@@ -0,0 +1,9 @@
+module Config.Banks (Bank(..), fintsEndpoint) where
+
+data Bank = ING | GLS | OTHER deriving (Eq, Show, Enum)
+
+fintsEndpoint :: Bank -> String
+fintsEndpoint = \case
+  ING -> "https://fints.ing.de/fints"
+  GLS -> "https://fints1.atruvia.de/cgi-bin/hbciservlet"
+  OTHER -> ""
